8d69cc5b336656cfaa720b88666a3491e91928da
Kyle <kylerutland@particledesign.com>
2017-04-12 11:54:05 -0700

    stable version 1

--- main.js
+++ main.js
@@ -3,142 +3,216 @@
 var app = {
 	self,
 //.................. config ...................
-	minIngredients: 3,
-	maxIngredients: 7,
-	ingredientAnimateInterval: 300,
+	numberOfIngredients:0,
+	weightedArray_NumberOfIngredientChances: [
+		{item:3, chance: .2},
+		{item:4, chance: .3},
+		{item:5, chance: .2},
+		{item:6, chance: .15},
+		{item:7, chance: .05},
+		{item:8, chance: .05},
+		{item:9, chance: .03},
+		{item:10, chance: .02}
+	],
 
+	weightedArray_IngredientTypeChances: [
+		{item:"meat", 		chance:.2},
+		{item:"sauce", 		chance:.2},
+		{item:"exterior", 	chance:.1},
+		{item:"topping", 	chance:.3},
+		{item:"filling", 	chance:.2},
+	],
+
+	ingredientAnimateInterval: 300,
+	
 //........... global variables .................
+	//meal ingredients 
 	meal:[],
-	mealDivs: [],
-	labelDivs: [],
-	//gather ingredients
+
+	//JSON ingredients
 	ingredientsArray:[], 
 		sauceArray: [], exteriorArray: [], meatArray: [], toppingArray: [], fillingArray: [],
-
-
 
 //..................................... application .............................................
 	init: function() {
 		self = this
+		//create Ingredient Arrays that can be pulled from with Recombinator  
 		this.fetchIngredientsFromJSON()
+		//set handlers for 
 		this.armRecombinateButton()
-
 	},
 
 	armRecombinateButton: function() {
-		$('div#recombinate_button').click(function(){
+		$('div#recombinate_button').on("click", function(){
+console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 			
-			$('div#meal_container').html("")	//clear previous meal view
-			$('div#label_container').html("")	//clear previous label view
-			self.recombinate()					//start new meal
+			$('div#recombinator').html("")	//clear previous meal view
+
+			self.recombinate()					//start new meal			
 		})
 	},
 
-
-	//fetch all the ingredients
+	//seperates Ingredient objects from JSON Array into a seperate array for each ingredient Type 
+	//self.sauceArray = [{ingredient, type, address},{etc.}], self.meatArray = [{etc.},{etc.}]
 	fetchIngredientsFromJSON : function(){
 
-		$.getJSON('ingredients.json', function( data ){ 
+		$.getJSON('ingredients.json', function( JSONarray ){ 
 
-			self.cacheIngredients(data)
+			JSONarray.forEach(function(item) {
+				
+				var currentIngredientType = item.type;
 
-		})
-	},
-	//save ingredients in seperate lists
-	cacheIngredients : function( JSONarray ){
-		JSONarray.forEach(function(item) {
+				if 		(currentIngredientType === "sauce") 	self.sauceArray.push(item)
+				else if (currentIngredientType === "exterior") 	self.exteriorArray.push(item)
+				else if (currentIngredientType === "meat") 		self.meatArray.push(item)
+				else if (currentIngredientType === "topping") 	self.toppingArray.push(item)
+				else if (currentIngredientType === "filling") 	self.fillingArray.push(item)
 			
-			var currentIngredientType = item.type,
-				currentIngredient = item.ingredient;
-
-			if (currentIngredientType === "sauce") 		self.sauceArray.push(currentIngredient)
-			if (currentIngredientType === "exterior") 	self.exteriorArray.push(currentIngredient)
-			if (currentIngredientType === "meat") 		self.meatArray.push(currentIngredient)
-			if (currentIngredientType === "topping") 	self.toppingArray.push(currentIngredient)
-			if (currentIngredientType === "filling") 	self.fillingArray.push(currentIngredient)
-		
-			self.ingredientsArray.push(currentIngredient)
+				self.ingredientsArray.push(item)
+			})
 		})
-
-		this.recombinate()
 	},
 
-
-//.......................... make meal from random ingredients .............................
+//.......................... create meal model from random ingredients .............................
 	recombinate : function() {
-		var exteriorArray = this.exteriorArray,
-			exterior = this.randomPicker(exteriorArray),
-			randomNumber = this.generateNumberOfIngredients();
+			//clear previous meal model and add an exterior
+			self.meal = []
+			//add exterior to model			
+			self.meal.push(self.pickRandomFromArray(self.exteriorArray)) 	
+			//determine number of ingredients
+			self.numberOfIngredients = self.returnRandomWeightedItemFromArray(self.weightedArray_NumberOfIngredientChances)
 
-			self.meal = []				//clear previous meal model
-			self.mealDivs = []			//clear previous meal model
-			self.labelDivs = []			//clear previous label model
-			self.meal.push( exterior ) 	//add random exterior to meal
+			//pick remaining ingredients
+			var determineIngredientType, randomIngredient,
+				i=1;
+			for (i; i<self.numberOfIngredients; i++) { 
 
-			for (var i=1; i<randomNumber; i++) { //add the rest of the ingredients to meal
-				var randomIngredient = self.randomPicker(self.ingredientsArray)//send the ingredients array to the randomizer
-				self.meal.push(randomIngredient)
+				//determine which type of ingredient will be chosen
+				determineIngredientType = self.returnRandomWeightedItemFromArray(self.weightedArray_IngredientTypeChances)
+				//then randomly choose an ingredient from the list of determined type
+				if 		(determineIngredientType === "sauce") 		randomIngredient = self.pickRandomFromArray(self.sauceArray)
+				else if (determineIngredientType === "exterior") 	randomIngredient = self.pickRandomFromArray(self.exteriorArray)
+				else if (determineIngredientType === "meat") 		randomIngredient = self.pickRandomFromArray(self.meatArray)
+				else if (determineIngredientType === "topping") 	randomIngredient = self.pickRandomFromArray(self.toppingArray)
+				else if (determineIngredientType === "filling") 	randomIngredient = self.pickRandomFromArray(self.fillingArray)
+
+				//add every ingredient object chosen to the meal
+				self.meal.push(randomIngredient) 
 			}
 
-// console.log(self.meal)
-		this.constructMealDivs();
+		self.displayMealDivs();
 	},
-		randomPicker : function(array) {
+
+		pickRandomFromArray : function(array) {
 			return array[Math.floor(Math.random()*array.length)]
 		},
-		generateNumberOfIngredients : function() {
-			var min = self.minIngredients, max = self.maxIngredients;
-			return Math.floor(Math.random() * (max - min + 1) + min)
+		returnRandomWeightedItemFromArray : function(weightedArray) {
+		
+			//create an array of ranges from weightArray to be rolled on with Math.random()
+			var weightRangesArray = [],
+				currentWeightMax=0;
+
+			weightedArray.forEach(function(object, i) {
+				currentWeightMax+=object.chance
+				weightRangesArray.push(currentWeightMax)
+			})
+
+			//roll random number, find which range it fits into, return the weightedArray item that range is associated with
+			var i=0,
+				random = Math.random();
+
+			while (i<weightRangesArray.length) {
+				if(random<=weightRangesArray[i]) {	
+					return weightedArray[i].item
+					break
+				}
+				i++
+			}
 		},
 
+//................................... display meal model in html ...................................
+	displayMealDivs: function() {
 
-//................................... make meal into html ...................................
-	constructMealDivs: function() {
-		var div, divs = '';
+		var 
+			//pass to animateMealDivs()
+			mealContainerIDs = [],
 
-		//construct Meal Divs
-		this.meal.forEach(function(item, number) {
-			div = "<div id='ingredient"+number+"' class='ingredient'>" + item + "</div>"
+			currentPictureDiv, currentLabelDiv, currentIngredientContainerID, currentIngredientContainerStyle, currentIngredientHTML,
+			z=1;
+		self.meal.forEach(function(ingredient, i) {
 
-			self.mealDivs.push(div)
+			currentPictureDiv = "<div id='ingredient_picture"+i+"' class='ingredient_picture'><img class='ingredient_img' src=" + ingredient.address + "></div>"
+			currentLabelDiv = "<div id='ingredient_label"+i+"' class='label'>" + ingredient.ingredient + '<div class="line"></div>'+"</div>"
+			//container ID
+			currentIngredientContainerID = 'ingredient_container'+i,
+			currentIngredientContainerStyle = "z-index:"+(z+=1),
+			currentIngredientHTML = "<div id="+currentIngredientContainerID+" class='ingredient_container hidden' style="+currentIngredientContainerStyle+">"
+				//ingredient label
+				+"<div class='label_container'>"
+					+"<div class='label_container2'>"
+						+currentLabelDiv
+					+"</div>"
+				+"</div>"
+				//ingredient picture
+				+"<div class='picture_container'>"
+					+currentPictureDiv
+				+"</div>"
+			+"</div>";
+
+			$('div#recombinator').prepend(currentIngredientHTML)
+			
+			//create mealContainerIDs array to pass to animateDivs()
+			mealContainerIDs.push(currentIngredientContainerID)
 		})
-		//construct Label Divs
-		this.meal.forEach(function(item, number) {
-			div = "<div id='label"+number+"' class='labels'>" + item + "</div>"
 
-			self.labelDivs.push(div)
-		})
-
-		this.displayMeal()
-		// this.displayLabels()
-		
+		//animate each ingredient Div one by one
+		self.animateMealDivs(mealContainerIDs)
 	},
-	displayMeal : function() {
-		var interval, currentIngredient;
 
-		this.labelDivs.forEach(function(item, i){
-			console.log(item)
+	animateMealDivs: function(mealContainerIDs) {
 
-			$('div#label_container').prepend('<div class="line">').prepend(item)
+		var mealContainerHeight = $(window).height() - $('#buttons').height();
+
+		//move each ingredient into view
+		mealContainerIDs.forEach(function(containerID, i) {
+
+			var $currentIngredient = $("div#"+containerID),
+				delay = self.ingredientAnimateInterval * i;
+
+			//push all ingredients offscreen above window
+			$currentIngredient.transition({ y:mealContainerHeight*-1 },0)
+
+			//animate each ingredient into place one at a time
+			setTimeout(function() {
+
+				$currentIngredient.transition({
+					y:0
+				})
+
+			}, delay)
 
 		})
 	},
-	// displayLabels: function(){
-	// 	var l = this.mealDivs.length,
-	// 		timeout = l*self.ingredientAnimateInterval;
 
-
-	// 	//set timer
-	// 	setTimeout(function(){
-	// 		self.mealDivs.forEach(function(item) {
-	// 			$('div#label_container').prepend(item)
-	// 		})
-
-	// 	}, timeout)
-
-	// }
 
 
 }
-
 app.init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
